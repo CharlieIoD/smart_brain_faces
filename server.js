7ae5cc71fa92bd36e647186591a1b956a4d3d5aa
CharlieIoD <charles_a_efford@hotmail.com>
2020-06-14 11:27:21 +0100

    Adding host path

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const imageurl = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-cubic-42275',
-      user : 'postgres',
-      password : '',
-      database : 'smartbrain'
+    host : 'process.env.DATABASE_URL',
+    ssl: true
     }
   });
 

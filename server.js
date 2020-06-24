29499ee50a6d876e64a63b4406d320265a279260
CharlieIoD <charles_a_efford@hotmail.com>
2020-06-24 17:29:32 +0100

    Database URL

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const profile = require('./controllers/profile');
 const db = knex({
     client:'pg',
     connection:{
-        host: 'postgresql-cubic-42275',
-        user: 'postgres',
-        password: 'postgres',
-        database: 'smartbrain'
+        host: 'process.env.DATABASE_URL',
+       ssl: true
     }
 });
 

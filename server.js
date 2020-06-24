3b1da6e6034f3f5ff88da2afb325d6f3d0ba920e
CharlieIoD <charles_a_efford@hotmail.com>
2020-06-24 20:48:06 +0100

    Another try

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const db = knex({
     client:'pg',
     connection:{
         connectionString : 'process.env.DATABASE_URL',
-        // ssl: {
-        //     rejectUnauthorized: false
-        //   }
+        ssl: {
+            rejectUnauthorized: false
+          }
         }
 });
 

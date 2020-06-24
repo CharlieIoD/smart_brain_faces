e2785af0488b0ac795b7fc896c44ca3e56efa94f
CharlieIoD <charles_a_efford@hotmail.com>
2020-06-24 17:21:01 +0100

    Post Gres

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const profile = require('./controllers/profile');
 const db = knex({
     client:'pg',
     connection:{
-        host: '127.0.0.1',
+        host: 'postgresql-cubic-42275',
         user: 'postgres',
         password: 'postgres',
         database: 'smartbrain'

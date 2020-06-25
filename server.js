a31565295d564e75a577ee6bc171e61690cb4db2
CharlieIoD <charles_a_efford@hotmail.com>
2020-06-25 14:37:47 +0100

    Add Image Require

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { response } = require('express');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
@@ -33,7 +34,7 @@ app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcryp
 
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 
-app.put('/imageurl', (req, res) => {image.handleImage(req, res, db)})
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 
 app.post('/image', (req, res) => {image.handleApiCall(req, res)})
 

260969054ec0e98b1390ea4a12f2c7e0eea3916a
CharlieIoD <charles_a_efford@hotmail.com>
2020-06-25 14:32:17 +0100

    Image

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcryp
 
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 
-app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.put('/imageurl', (req, res) => {image.handleImage(req, res, db)})
 
 app.post('/image', (req, res) => {image.handleApiCall(req, res)})
 
